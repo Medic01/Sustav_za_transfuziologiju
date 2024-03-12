cf8af7b86bca52f31b425c5aaec69339fd59337a
Medic01 <94714313+Medic01@users.noreply.github.com>
2024-03-12 11:53:59 +0100

    Spajanje na firebase

---
+++ lib/Prijava.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PrijavaPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Prijava'),
+      ),
+      body: Center(
+        child: Text('Ovo je stranica za prijavu.'),
+      ),
+    );
+  }
+}
---
+++ lib/Registracija.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegistracijaPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Registracija'),
+      ),
+      body: Center(
+        child: Text('Ovo je stranica za registraciju.'),
+      ),
+    );
+  }
+}
