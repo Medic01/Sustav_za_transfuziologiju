7adbd5e441f1037d15004fa24cc3a912dc1da974
Medic01 <94714313+Medic01@users.noreply.github.com>
2024-03-18 09:38:03 +0100

    added options for amount of donated blood, blood passed control(boolean), blood is donated(boolead)

--- lib/recordTheDoze.dart
+++ lib/recordTheDoze.dart
@@ -4,6 +4,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class RecordTheDoze extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    TextEditingController quantityController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Accepted bloods'),
@@ -36,7 +38,126 @@ class RecordTheDoze extends StatelessWidget {
                     Text('hemoglobin ${data['hemoglobin']}'),
                     Text('name_of_doctor ${data['name_of_doctor']}'),
                     Text('status ${data['status']}'),
-                    Text(' user_id ${data['user_id']}'),
+                    Text('user_id ${data['user_id']}'),
+                    // New options
+                    Text(
+                      'Doza obrađena: ${data['doza_obradjena']}',
+                      style: TextStyle(
+                          color: data['doza_obradjena'] == true
+                              ? Colors.green
+                              : Colors.black),
+                    ),
+                    Text(
+                      'Doza iskorištena: ${data['doza_iskoristena']}',
+                      style: TextStyle(
+                          color: data['doza_iskoristena'] == true
+                              ? Colors.green
+                              : Colors.black),
+                    ),
+                    Text(
+                      'Količina donirane doze: ${data['kolicina_donirane_doze'] ?? 'N/A'}',
+                      style: TextStyle(
+                          color: data['kolicina_donirane_doze'] == 100
+                              ? Colors.red
+                              : Colors.green),
+                    ),
+                  ],
+                ),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        // Dodavanje logike za označavanje doze kao obrađene
+                        FirebaseFirestore.instance
+                            .collection('accepted')
+                            .doc(document.id)
+                            .update({
+                          'doza_obradjena': true,
+                        }).then((value) {
+                          print('Doza je obrađena');
+                        }).catchError((error) {
+                          print(
+                              'Greška prilikom označavanja doze kao obrađene: $error');
+                        });
+                      },
+                      child: Text('Označi kao obrađeno'),
+                    ),
+                    SizedBox(width: 8),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Dodavanje logike za označavanje doze kao iskorištene
+                        FirebaseFirestore.instance
+                            .collection('accepted')
+                            .doc(document.id)
+                            .update({
+                          'doza_iskoristena': true,
+                        }).then((value) {
+                          print('Doza je iskorištena');
+                        }).catchError((error) {
+                          print(
+                              'Greška prilikom označavanja doze kao iskorištene: $error');
+                        });
+                      },
+                      child: Text('Označi kao iskorišteno'),
+                    ),
+                    SizedBox(width: 8),
+                    ElevatedButton(
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text('Unesite količinu donirane doze'),
+                              content: TextField(
+                                controller: quantityController,
+                                keyboardType: TextInputType.number,
+                                decoration: InputDecoration(
+                                  labelText: 'Količina',
+                                ),
+                              ),
+                              actions: [
+                                ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: Text('Odustani'),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    String quantity = quantityController.text;
+                                    if (quantity.isNotEmpty &&
+                                        int.tryParse(quantity) != null &&
+                                        int.parse(quantity) >=
+                                            (data['kolicina_donirane_doze'] ??
+                                                0)) {
+                                      FirebaseFirestore.instance
+                                          .collection('accepted')
+                                          .doc(document.id)
+                                          .update({
+                                        'kolicina_donirane_doze':
+                                            int.parse(quantity),
+                                      }).then((value) {
+                                        print(
+                                            'Količina donirane doze je unesena');
+                                        Navigator.pop(context);
+                                      }).catchError((error) {
+                                        print(
+                                            'Greška prilikom unosa količine donirane doze: $error');
+                                      });
+                                    } else {
+                                      print('Neispravan unos količine');
+                                    }
+                                  },
+                                  child: Text('Potvrdi'),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                      child: Text('Unesi količinu'),
+                    ),
                   ],
                 ),
               );
