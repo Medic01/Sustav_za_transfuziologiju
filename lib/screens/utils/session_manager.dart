9fa725452d21820bf56911551cc7268da17cbb3d
Jozo Spajic <j.spajic@globalsoft.ba>
2024-03-25 14:47:07 +0100

    - sessionmanager added

---
+++ lib/screens/utils/session_manager.dart
@@ -0,0 +1,27 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SessionManager {
+  static const userIdKey = 'user_id';
+
+  late final String userId ;
+  late final SharedPreferences preferences;
+
+  Future<void> setUserId(String userId) async {
+    preferences = await SharedPreferences.getInstance();
+    preferences.setString(userIdKey, userId);
+  }
+
+  Future<String?> getUserId() async {
+    preferences = await SharedPreferences.getInstance();
+    
+    String? userId;
+    userId = preferences.getString(userIdKey);
+
+    return userId;
+  }
+// TODO -> set this on logout
+  clearAll() async {
+    preferences = await SharedPreferences.getInstance();
+    await preferences.clear();
+  }
+}
