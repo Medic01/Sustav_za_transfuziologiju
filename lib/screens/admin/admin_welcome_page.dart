f7eb87e9c971c45d5ccd7e5bf29a2ddb7d4b76b6
AntonioPavkovic <antoniopavkovic999@gmail.com>
2024-03-26 09:46:42 +0100

    add filtering on admin page if the blood donation is accepted or rejected

--- lib/screens/admin/admin_welcome_page.dart
+++ lib/screens/admin/admin_welcome_page.dart
@@ -1,25 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class AdminWelcomePage extends StatelessWidget {
-  const AdminWelcomePage({super.key});
+class AdminWelcomePage extends StatefulWidget {
+  @override
+  _AdminWelcomePageState createState() => _AdminWelcomePageState();
+}
 
+class _AdminWelcomePageState extends State<AdminWelcomePage> {
+  String _selectedFilter = 'Accepted';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Admin Home Page'),
+        title: const Text('Admin Početna Stranica'),
       ),
-
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Welcome to the admin page!',
-              style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold
+            SizedBox(height: 20.0),
+            DropdownButton<String>(
+              value: _selectedFilter,
+              onChanged: (String? newValue) {
+                setState(() {
+                  _selectedFilter = newValue!;
+                });
+              },
+              items: <String>['Accepted', 'Rejected'].map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 20.0),
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: _selectedFilter == 'Accepted'
+                    ? FirebaseFirestore.instance.collection('accepted').snapshots()
+                    : FirebaseFirestore.instance.collection('rejected').snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  }
+
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                      return CircularProgressIndicator();
+                    default:
+                      return ListView.builder(
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, index) {
+                          DocumentSnapshot document = snapshot.data!.docs[index];
+                          if (_selectedFilter == 'Accepted') {
+                            String donorName = document['donor_name'];
+                            String bloodType = document['blood_type'];
+                            int? donatedAmount = document['donated_dose'];
+
+                            return Card(
+                              child: ListTile(
+                                title: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Donor Name: $donorName'),
+                                    Text('Blood Type: $bloodType'),
+                                    if (donatedAmount != null)
+                                      Text('Donated Amount: $donatedAmount'),
+                                  ],
+                                ),
+                              ),
+                            );
+                          } else {
+                            String name = document['donor_name'];
+                            String bloodType = document['blood_type'];
+                            String rejectionReason = document['reason_for_rejection'];
+
+                            return Card(
+                              child: ListTile(
+                                title: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Name: $name'),
+                                    Text('Blood Type: $bloodType'),
+                                    Text('Rejection Reason: $rejectionReason'),
+                                  ],
+                                ),
+                              ),
+                            );
+                          }
+                        },
+                      );
+                  }
+                },
               ),
             ),
           ],
