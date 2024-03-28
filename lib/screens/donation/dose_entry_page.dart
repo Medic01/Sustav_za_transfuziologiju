12be01f978af01f5d5581e3275d8a1683deebfe7
AntonioPavkovic <antoniopavkovic999@gmail.com>
2024-03-28 09:59:20 +0100

    finished donation data modeling and seperating firebase functionality to another file

--- lib/screens/donation/dose_entry_page.dart
+++ lib/screens/donation/dose_entry_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:sustav_za_transfuziologiju/screens/enums/blood_types.dart';
+import 'package:sustav_za_transfuziologiju/services/donation_service.dart';
+
+import 'donation_list_item.dart';
 
 class DoseEntryPage extends StatefulWidget {
-  const DoseEntryPage({super.key});
+  const DoseEntryPage({Key? key}) : super(key: key);
 
   @override
   _DoseEntryPageState createState() => _DoseEntryPageState();
@@ -10,11 +14,11 @@ class DoseEntryPage extends StatefulWidget {
 
 class _DoseEntryPageState extends State<DoseEntryPage> {
   String? selectedBloodType;
+  final DonationService _donationService = DonationService();
+  final TextEditingController quantityController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    TextEditingController quantityController = TextEditingController();
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Accepted blood donations'),
@@ -22,207 +26,67 @@ class _DoseEntryPageState extends State<DoseEntryPage> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          DropdownButtonFormField<String>(
-            decoration: const InputDecoration(
-              labelText: 'Choose blood type',
-              border: OutlineInputBorder(),
-            ),
-            value: selectedBloodType,
-            onChanged: (String? newValue) {
-              setState(() {
-                selectedBloodType = newValue;
-              });
-            },
-            items: <String>[
-              'All',
-              'A_POSITIVE',
-              'A_NEGATIVE',
-              'B_POSITIVE',
-              'B_NEGATIVE',
-              'AB_POSITIVE',
-              'AB_NEGATIVE',
-              'O_POSITIVE',
-              'O_NEGATIVE'
-            ].map((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-          ),
+          buildDropdownButtonFormField(),
           const SizedBox(height: 8),
           Expanded(
-            child: StreamBuilder(
-              stream: selectedBloodType == 'All'
-                  ? FirebaseFirestore.instance
-                      .collection('accepted')
-                      .snapshots()
-                  : FirebaseFirestore.instance
-                      .collection('accepted')
-                      .where('blood_type', isEqualTo: selectedBloodType)
-                      .snapshots(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (snapshot.hasError) {
-                  return Text('An error occurred: ${snapshot.error}');
-                }
-
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-
-                return ListView(
-                  shrinkWrap: true,
-                  children:
-                      snapshot.data!.docs.map((DocumentSnapshot document) {
-                    Map<String, dynamic> data =
-                        document.data() as Map<String, dynamic>;
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        ListTile(
-                          title: Text('Lokacija donacije: ${data['location']}'),
-                          subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('Datum donacije: ${data['date']}'),
-                              Text('Krvni tlak davatelja: ${data['blood_pressure']}'),
-                              Text('Hemoglobin: ${data['hemoglobin']}'),
-                              Text('Ime doktora: ${data['doctor_name']}'),
-                              Text('Krvna grupa: ${data['blood_type']}'),
-                              Text('Ime davatelja: ${data['donor_name']}'),
-                              Text('Krv obrađena: ${data['dose_processed']}',
-                                style: TextStyle(
-                                    color: data['dose_processed'] == true
-                                        ? Colors.green
-                                        : Colors.black),
-                              ),
-                              Text('Krv iskorištena: ${data['dose_used']}',
-                                style: TextStyle(
-                                    color: data['dose_used'] == true
-                                        ? Colors.green
-                                        : Colors.black),
-                              ),
-                              Text('Količina donirana: ${data['donated_dose'] ?? 'N/A'}',
-                                style: TextStyle(
-                                    color: data['donated_dose'] == 100
-                                        ? Colors.red
-                                        : Colors.green),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            ElevatedButton(
-                              onPressed: () {
-                                FirebaseFirestore.instance
-                                    .collection('accepted')
-                                    .doc(document.id)
-                                    .update({
-                                  'dose_processed': true,
-                                }).then((value) {
-                                  print('Doza je obrađena');
-                                }).catchError((error) {
-                                  print('Greška prilikom označavanja doze kao obrađene: $error');
-                                });
-                              },
-                              child: const Text('Obrađena'),
-                            ),
-                            ElevatedButton(
-                              onPressed: () {
-                                FirebaseFirestore.instance
-                                    .collection('accepted')
-                                    .doc(document.id)
-                                    .update({
-                                  'dose_used': true,
-                                }).then((value) {
-                                  print('Doza je iskorištena');
-                                }).catchError((error) {
-                                  print('Greška prilikom označavanja doze kao iskorištene: $error');
-                                });
-                              },
-                              child: const Text('Iskorišteno'),
-                            ),
-                            ElevatedButton(
-                              onPressed: () {
-                                showDialog(
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    return AlertDialog(
-                                      title: const Text('Unesite količinu donirane doze'),
-                                      content: TextField(
-                                        controller: quantityController,
-                                        keyboardType: TextInputType.number,
-                                        decoration: const InputDecoration(
-                                          labelText: 'Količina',
-                                        ),
-                                      ),
-                                      actions: [
-                                        ElevatedButton(
-                                          onPressed: () {
-                                            Navigator.pop(context);
-                                          },
-                                          child: const Text('Odustani'),
-                                        ),
-                                        ElevatedButton(
-                                          onPressed: () {
-                                            String quantity =
-                                                quantityController.text;
-                                            int existingQuantity = data['donated_dose'] ??
-                                                0;
-                                            if (quantity.isNotEmpty &&
-                                                int.tryParse(quantity) != null
-                                                && int.parse(quantity) >= existingQuantity
-                                                && int.parse(quantity) <= 100) {
-                                              FirebaseFirestore.instance
-                                                  .collection('accepted')
-                                                  .doc(document.id)
-                                                  .update({
-                                                'donated_dose':
-                                                    int.parse(quantity),
-                                              }).then((value) {
-                                                print('Količina donirane doze je unesena');
-                                                Navigator.pop(context);
-                                              }).catchError((error) {
-                                                print('Greška prilikom unosa količine donirane doze: $error');
-                                              });
-                                            } else if (int.parse(quantity) < existingQuantity) {
-                                              print('Ne možete unijeti manju količinu od postojeće');
-                                              ScaffoldMessenger.of(context)
-                                                  .showSnackBar(const SnackBar(
-                                                content: Text('Ne možete unijeti manju količinu od postojeće'),
-                                              ));
-                                            } else {
-                                              print('Neispravan unos količine');
-                                              ScaffoldMessenger.of(context)
-                                                  .showSnackBar(SnackBar(
-                                                content: Text('Količina mora biti između $existingQuantity i 100'),
-                                              ));
-                                            }
-                                          },
-                                          child: const Text('Potvrdi'),
-                                        ),
-                                      ],
-                                    );
-                                  },
-                                );
-                              },
-                              child: const Text('Količina'),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 8),
-                      ],
-                    );
-                  }).toList(),
-                );
-              },
-            ),
+            child: buildDonationList(),
           ),
         ],
       ),
+    );
+  }
+
+  Widget buildDropdownButtonFormField() {
+    return DropdownButtonFormField<String>(
+      decoration: const InputDecoration(
+        labelText: 'Choose blood type',
+        border: OutlineInputBorder(),
+      ),
+      value: selectedBloodType,
+      onChanged: (String? newValue) {
+        setState(() {
+          selectedBloodType = newValue;
+        });
+      },
+      items: BloodTypes.values.map((BloodTypes value) {
+        return DropdownMenuItem<String>(
+          value: value.toString().split('.').last,
+          child: Text(value.toString().split('.').last),
+        );
+      }).toList(),
+    );
+  }
+
+  Widget buildDonationList() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: selectedBloodType == 'All'
+          ? FirebaseFirestore.instance.collection('accepted').snapshots()
+          : FirebaseFirestore.instance
+          .collection('accepted')
+          .where('blood_type', isEqualTo: selectedBloodType)
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('An error occurred: ${snapshot.error}');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        return ListView(
+          shrinkWrap: true,
+          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+            Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+            return DonationListItem(
+              data: data,
+              documentId: document.id,
+              donationService: _donationService,
+              quantityController: quantityController,
+            );
+          }).toList(),
+        );
+      },
     );
   }
 }
--- lib/services/donation_service.dart
+++ lib/services/donation_service.dart
@@ -75,4 +75,102 @@ class DonationService {
     }
   }
 
+  Future<void> acceptDonationAfterReservation(String documentId, Map<String, dynamic> data) async {
+    try {
+      CollectionReference donationRef = _db.collection('donation_date');
+      CollectionReference acceptedRef = _db.collection('accepted');
+
+      await acceptedRef.add({
+        'location': data['location'],
+        'date': data['date'],
+        'blood_pressure': data['blood_pressure'],
+        'hemoglobin': data['hemoglobin'],
+        'doctor_name': data['doctor_name'],
+        'blood_type': data['blood_type'],
+        'user_id': data['user_id'],
+        'donor_name': data['donor_name'],
+      });
+
+      await donationRef.doc(documentId).delete();
+    } catch (e) {
+      print('Error $e has occured!');
+      throw e;
+    }
+  }
+
+  Future<void> rejectDonationAfterReservation(String documentId, Map<String, dynamic> data, String rejectionReason) async {
+    try {
+      CollectionReference donationRef = _db.collection('donation_date');
+      CollectionReference rejectedRef = _db.collection('rejected');
+
+      await rejectedRef.add({
+        'location': data['location'],
+        'date': data['date'],
+        'blood_pressure': data['blood_pressure'],
+        'hemoglobin': data['hemoglobin'],
+        'doctor_name': data['doctor_name'],
+        'blood_type': data['blood_type'],
+        'user_id': data['user_id'],
+        'donor_name': data['donor_name'],
+        'reason_for_rejection': rejectionReason,
+      });
+
+      await donationRef.doc(documentId).update({
+        'donation_rejected': true,
+        'reason_for_rejection': rejectionReason,
+      });
+
+      await donationRef.doc(documentId).delete();
+    } catch (e) {
+      print('Error $e has occurred!');
+      throw e;
+    }
+  }
+
+  Stream<List<Donation>> getAcceptedDonations(String? selectedBloodType) {
+    Query collectionRef = _db.collection('accepted');
+
+    if (selectedBloodType != null && selectedBloodType != 'All') {
+      collectionRef = collectionRef.where('blood_type', isEqualTo: selectedBloodType);
+    }
+
+    return collectionRef.snapshots().map((snapshot) =>
+          snapshot.docs.map((doc) => Donation.fromFirestore(doc)).toList());
+  }
+
+  Future<void> markDoseProcessed(String donationId) async {
+    try {
+      await _db.collection('accepted').doc(donationId).update({
+        'dose_processed': true,
+      });
+
+      print("Dose marked as processed!");
+    } catch (e) {
+      print("Error occurred while trying to mark dose as processed: $e");
+    }
+  }
+
+  Future<void> markDoseUsed(String donationId) async {
+    try {
+      await _db.collection('accepted').doc(donationId).update({
+        'dose_used': true,
+      });
+      print('Dose marked as used');
+    } catch (e) {
+      print('Error occurred while marking dose as used: $e');
+      throw e;
+    }
+  }
+
+  Future<void> updateDonatedDose(String donationId, int newQuantity) async {
+    try {
+      await _db.collection('accepted').doc(donationId).update({
+        'donated_dose': newQuantity,
+      });
+      print('Donated dose quantity updated');
+    } catch (e) {
+      print('Error occurred while updating donated dose quantity: $e');
+      throw e;
+    }
+  }
 }
