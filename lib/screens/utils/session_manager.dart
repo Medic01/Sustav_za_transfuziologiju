bb4f274df2d867cd151d3c1be6bbcb485166fc33
Medic01 <94714313+Medic01@users.noreply.github.com>
2024-03-28 14:35:46 +0100

    now it gets uesrId after registration using session manager

--- lib/screens/utils/session_manager.dart
+++ lib/screens/utils/session_manager.dart
@@ -3,7 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SessionManager {
   static const userIdKey = 'user_id';
 
-  late final String userId ;
+  late final String userId;
   late final SharedPreferences preferences;
 
   Future<void> setUserId(String userId) async {
@@ -13,15 +13,18 @@ class SessionManager {
 
   Future<String?> getUserId() async {
     preferences = await SharedPreferences.getInstance();
-    
+
     String? userId;
     userId = preferences.getString(userIdKey);
 
     return userId;
   }
+
 // TODO -> set this on logout
   clearAll() async {
     preferences = await SharedPreferences.getInstance();
     await preferences.clear();
   }
+
+  setUserLoggedIn(bool bool) {}
 }
