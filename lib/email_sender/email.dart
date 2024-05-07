55b08039d687091bbab6cc99b156a09f54e22691
Medic01 <94714313+Medic01@users.noreply.github.com>
2024-05-07 16:52:02 +0200

    finished emailSender

--- lib/email_sender/email.dart
+++ lib/email_sender/email.dart
@@ -1,5 +1,6 @@
 import 'package:mailer/mailer.dart';
 import 'package:mailer/smtp_server.dart';
+import 'package:sustav_za_transfuziologiju/email_sender/er.dart';
 
 Future<void> sendBloodDonationEmail(
     String recipientEmail, bool accepted) async {
@@ -17,41 +18,38 @@ Future<void> sendBloodDonationEmail(
   final message = Message()
     ..from = const Address(username, 'Transfuzija Krvi')
     ..recipients.add(recipientEmail)
-    ..subject = accepted ? 'Prihvaćena donacija krvi' : 'Odbijena donacija krvi'
-    ..text = accepted
-        ? 'Vaša donacija krvi je uspješno prihvaćena. Hvala vam što ste donirali.'
-        : 'Nažalost, vaša donacija krvi nije prihvaćena.';
+    ..subject = accepted ? acceptedSubject : rejectedSubject
+    ..text = accepted ? acceptedBloodText : rejectedBloodText;
 
   try {
     final sendReport = await send(message, smtpServer);
 
     if (sendReport != null) {
-      print('Email poslan: ${sendReport.toString()}');
-
+      print('$emailSent ${sendReport.toString()}');
       await Future.delayed(const Duration(minutes: 2));
 
       final additionalMessage = Message()
         ..from = const Address(username, 'Transfuzija Krvi')
         ..recipients.add(recipientEmail)
-        ..subject = 'Dodatna poruka'
-        ..text = 'Evo me nakon 2 minute.';
+        ..subject = additionalMessageText
+        ..text = additionaMessageanotherText;
 
       final additionalSendReport = await send(additionalMessage, smtpServer);
 
       if (additionalSendReport != null) {
-        print('Dodatni email poslan: ${additionalSendReport.toString()}');
+        print('$additionalEmailSent ${additionalSendReport.toString()}');
       } else {
-        print('Dodatni email nije uspješno poslan');
+        print(additionalEmailNotSent);
       }
     } else {
-      print('Email nije uspješno poslan');
+      print(emailNotSent);
     }
   } on MailerException catch (e) {
-    print('Greška pri slanju emaila: ${e.message}');
+    print('$emailSentError ${e.message}');
     for (var p in e.problems) {
-      print('Problem: ${p.code}: ${p.msg}');
+      print('$problem ${p.code}: ${p.msg}');
     }
   } catch (e) {
-    print('Nepoznata greška: $e');
+    print('$unknownError $e');
   }
 }
---
+++ lib/email_sender/er.dart
@@ -0,0 +1,14 @@
+String acceptedSubject = "Prihvaćena donacija krvi";
+String rejectedSubject = "Odbijena donacija krvi";
+String acceptedBloodText =
+    "Vaša donacija krvi je uspješno prihvaćena. Hvala vam što ste donirali.";
+String rejectedBloodText = "Nažalost, vaša donacija krvi nije prihvaćena.";
+String emailSent = "Email poslan:";
+String additionalMessageText = "Pristupite darivanju krvi ponovo";
+String additionaMessageanotherText = "Darivajte krv ponovo za 3 mjeseca.";
+String additionalEmailSent = "Dodatni email poslan:";
+String additionalEmailNotSent = "Dodatni email nije uspješno poslan";
+String emailNotSent = "Email nije uspješno poslan";
+String emailSentError = "Greška pri slanju emaila: ";
+String problem = "Problem: ";
+String unknownError = "Nepoznata greška: ";
